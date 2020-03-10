cca1e3825a3a96c4053e72e640cd7219c6a19550
Parker <chrispar@sheridancollege.ca>
2020-03-10 14:10:09 -0400

    Added Xcode Project

---
+++ GlacoOrderApp/GlacoOrderApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GlacoOrderApp
+//
+//  Created by Parker Christie on 2020-03-10.
+//  Copyright © 2020 GLAC Co. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
