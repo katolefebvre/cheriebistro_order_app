bcd63d6c15e59de348e63dd2413a847494089aff
Kato Lefebvre <klefebvre90@gmail.com>
2020-03-21 17:32:45 -0400

    Functionality to submit a menu item to remote database

--- GlacoOrderApp/GlacoOrderApp/ViewControllers/AddFoodItemViewController.swift
+++ GlacoOrderApp/GlacoOrderApp/ViewControllers/AddFoodItemViewController.swift
@@ -25,7 +25,31 @@ class AddFoodItemViewController: UIViewController, CategoryPopoverControllerDele
         // Do any additional setup after loading the view.
     }
     
-    @IBAction func sendToDB(_ sender: Any) {
+    @IBAction func displayPopover(_ sender: UIButton) {
+           let storyboard : UIStoryboard = UIStoryboard(name: "Main", bundle: nil)
+           let vc = storyboard.instantiateViewController(withIdentifier: "CategoryPopoverViewController") as! CategoryPopoverViewController
+           vc.modalPresentationStyle = .popover
+           vc.controllerDelegate = self
+           let popover: UIPopoverPresentationController = vc.popoverPresentationController!
+           popover.sourceView = sender
+           present(vc, animated: true, completion:nil)
+    }
+    
+    @IBAction func submitAddFoodItem(_ sender: Any) {
+        let alertBox = UIAlertController(title: "Add Food Item", message: "Are you sure you want to add \(tfName.text!) to the menu?", preferredStyle: .alert)
+        
+        let noAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+        let yesAction = UIAlertAction(title: "Confirm", style: .default, handler: { (alert) in
+            self.sendToDB()
+        })
+        
+        alertBox.addAction(noAction)
+        alertBox.addAction(yesAction)
+        
+        self.present(alertBox, animated: true, completion: nil)
+    }
+    
+    func sendToDB() {
         let address = URL(string: "http://142.55.32.86:50131/cheriebistro/api/addfooditem.php")!
         let url = NSMutableURLRequest(url: address)
         url.httpMethod = "POST"
@@ -64,15 +88,6 @@ class AddFoodItemViewController: UIViewController, CategoryPopoverControllerDele
             uploadJob.resume()
         }
     }
-    @IBAction func displayPopover(_ sender: UIButton) {
-        let storyboard : UIStoryboard = UIStoryboard(name: "Main", bundle: nil)
-        let vc = storyboard.instantiateViewController(withIdentifier: "CategoryPopoverViewController") as! CategoryPopoverViewController
-        vc.modalPresentationStyle = .popover
-        vc.controllerDelegate = self
-        let popover: UIPopoverPresentationController = vc.popoverPresentationController!
-        popover.sourceView = sender
-        present(vc, animated: true, completion:nil)
-    }
     
     func showError() {
         DispatchQueue.main.async {
@@ -80,21 +95,6 @@ class AddFoodItemViewController: UIViewController, CategoryPopoverControllerDele
             alert.addAction(UIAlertAction(title: "OK", style: .cancel, handler: nil))
             self.present(alert, animated: true, completion: nil)
         }
-    }
-    @IBAction func submitAddFoodItem(_ sender: Any) {
-        
-        
-        let alertBox = UIAlertController(title: "Add Food Item", message: "Are you sure you want to add \(nameTextField.text!) to the menu?", preferredStyle: .alert)
-        
-        let noAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
-        let yesAction = UIAlertAction(title: "Confirm", style: .default, handler: { (alert) in
-            // HANDLE ADDING OF FOOD ITEM
-        })
-        
-        alertBox.addAction(noAction)
-        alertBox.addAction(yesAction)
-        
-        self.present(alertBox, animated: true, completion: nil)
     }
     
     func addCategory(category: String) {
@@ -104,7 +104,4 @@ class AddFoodItemViewController: UIViewController, CategoryPopoverControllerDele
     func removeCategory(category: String) {
         categories.remove(at: (categories.firstIndex(of: category)!))
     }
-    
-    
-
 }
