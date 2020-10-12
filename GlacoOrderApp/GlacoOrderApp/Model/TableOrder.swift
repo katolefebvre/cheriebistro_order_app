7953c4bce2c06f869a9552cf2a1b6902a46aa0ac
Kato <klefebvre90@gmail.com>
2020-10-12 16:47:36 -0400

    Auto stash before merge of "master" and "origin/master"

---
+++ GlacoOrderApp/GlacoOrderApp/Model/TableOrder.swift
@@ -0,0 +1,20 @@
+//
+//  TableOrder.swift
+//  GlacoOrderApp
+//
+//  Created by Kato Lefebvre on 2020-10-11.
+//  Copyright © 2020 GLAC Co. All rights reserved.
+//
+
+import Foundation
+
+class TableOrder {
+    
+    var id: Int
+    var tableId: Int
+    
+    init(id: Int, tableId: Int) {
+        self.id = id
+        self.tableId = tableId
+    }
+}
---
+++ GlacoOrderApp/GlacoOrderApp/ViewControllers/TableOrdersViewController.swift
@@ -0,0 +1,16 @@
+//
+//  TableOrdersViewController.swift
+//  GlacoOrderApp
+//
+//  Created by Kato Lefebvre on 2020-10-11.
+//  Copyright © 2020 GLAC Co. All rights reserved.
+//
+
+import UIKit
+
+class TableOrdersViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
